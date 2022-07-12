4dcf9938dd6e67b7e77e1b67981c5bbb44b9d7c2
tatevkyosababyan <31800282+tatevkyosababyan@users.noreply.github.com>
2022-07-12 10:49:14 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='booklover',
+      version='1.0',
+      description='This is an attempt to create a python package',
+      author='Tatev Kyosababyan',
+      author_email='ers5af@virginia.edu',
+      url='https://github.com/tatevkyosababyan/mylib',
+      packages=['booklover']
+     )
